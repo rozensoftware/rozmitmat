3db8095c3ff94bda9b776f6fb3bdf709cb9bea86
Piotr Rozentreter <rozsoft@wp.pl>
2023-04-29 22:00:32 +0200

    wip

--- rozmitmatgui/src/main.rs
+++ rozmitmatgui/src/main.rs
@@ -1,8 +1,13 @@
 use eframe::egui;
+use memmap2::MmapMut;
+
+mod networkutils;
 
 const APP_NAME: &str = "Rozmitmat v0.1.0";
 const WINDOW_WIDTH: f32 = 800.0;
 const WINDOW_HEIGHT: f32 = 600.0;
+const MEMORY_MAP_FILE_NAME: &str = "output.mmap";
+const MEMORY_FILE_SIZE: usize = 1024 * 1024 * 5; // 5 MB
 
 fn main() -> Result<(), eframe::Error>
 {   
@@ -22,15 +27,49 @@ fn main() -> Result<(), eframe::Error>
 struct RozmitmatApp
 {
     console_output: String,
+    interface_name: String,
+    target_ip: String,
+    domain_name: String,
+    router_ip: String,
+    redirect_ip: String,
+    memoryfile: MmapMut,
 }
 
 impl Default for RozmitmatApp
 {
     fn default() -> Self 
     {
+        let ret = match networkutils::get_router_ip()
+        {
+            Ok(ip) => ip,
+            Err(_) => 
+            {
+                (String::new(), String::new())
+            }
+        };
+
+        let mem_mapped_file_name = std::env::current_exe().unwrap().parent().unwrap().to_str().unwrap().to_string() + "/" + MEMORY_MAP_FILE_NAME;
+
+        let file = std::fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(mem_mapped_file_name).unwrap();
+
+        file.set_len(MEMORY_FILE_SIZE as u64).unwrap();
+
+        let mut memoryfile = unsafe { MmapMut::map_mut(&file).unwrap() };
+        memoryfile[0] = 0;
+
         Self 
         {
-            console_output: "shdgshdsdsdggggggggggggggggg".to_string(),
+            console_output: String::new(),
+            interface_name: ret.1,
+            target_ip: String::new(),
+            domain_name: String::new(),   
+            router_ip: ret.0,
+            redirect_ip: String::new(),
+            memoryfile: memoryfile,
         }
     }
 }
@@ -42,20 +81,51 @@ impl eframe::App for RozmitmatApp
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| 
         {
             ui.heading(APP_NAME);
-            //render label centered in the panel
+
             ui.horizontal(|ui| 
             {
-                ui.centered_and_justified(|ui| 
-                {
-                    ui.label("Rozmitmat");
-                });
+                ui.label("Interface name:");
+                ui.add(egui::TextEdit::singleline(&mut self.interface_name));
+            });
+
+            ui.horizontal(|ui| 
+            {
+                ui.label("Target IP:");
+                ui.add(egui::TextEdit::singleline(&mut self.target_ip));
+            });
+
+            ui.horizontal(|ui| 
+            {
+                ui.label("Router IP:");
+                ui.add(egui::TextEdit::singleline(&mut self.router_ip));
+            });
+
+            ui.horizontal(|ui| 
+            {
+                ui.label("Domain name:");
+                ui.add(egui::TextEdit::singleline(&mut self.domain_name).hint_text("Domain name to spoof e.g. example.com"));
+            });
+
+            ui.horizontal(|ui| 
+            {
+                ui.label("Redirect to IP:");
+                ui.add(egui::TextEdit::singleline(&mut self.redirect_ip));
             });
         });
 
         egui::CentralPanel::default().show(ctx, |ui| 
         {
-            egui::Window::new("Console").resizable(true).scroll2([false, true]).show(ui.ctx(), |ui|
+            egui::Window::new("Output").resizable(true).scroll2([false, true]).show(ui.ctx(), |ui|
             {
+                if self.memoryfile[0] != 0
+                {
+                    self.console_output = String::from_utf8_lossy(&self.memoryfile).to_string();
+                }
+                else
+                {
+                    self.console_output = String::new();
+                }
+
                 let mut txt = self.console_output.as_str();
                 let console_txt = egui::TextEdit::multiline(&mut txt);//.min_size(egui::vec2(100.0, 100.0));
                 ui.add(console_txt);
---
+++ rozmitmatgui/src/networkutils.rs
@@ -0,0 +1,34 @@
+//read router ip from /etc/resolv.conf
+
+use std::process::Command;
+use std::io::Error as RetErr;
+use std::io::ErrorKind;
+
+pub fn get_router_ip() -> Result<(String, String), RetErr>
+{
+    //read output from ip route
+    let output = Command::new("ip")
+        .arg("route")
+        .output()
+        .expect("failed to execute command ip route");
+
+    //convert output to string
+    let output = String::from_utf8_lossy(&output.stdout);
+
+    //split output into lines
+    let lines: Vec<&str> = output.split("\n").collect();
+
+    //get the first line
+    let first_line = lines[0];
+
+    //split first line into words
+    let words: Vec<&str> = first_line.split(" ").collect();
+
+    if words.len() < 5
+    {
+        return Err(std::io::Error::new(ErrorKind::Other, "Failed to parse ip route output"));
+    }
+
+    //return the second word
+    Ok((words[2].to_string(), words[4].to_string()))
+}
