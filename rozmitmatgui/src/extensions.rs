2b2d945414957ae1dfd24edac9767bebf5bfe2e6
Piotr Rozentreter <rozsoft@wp.pl>
2023-05-05 11:39:23 +0200

    proxy fixes

--- rozmitmatgui/src/extensions.rs
+++ rozmitmatgui/src/extensions.rs
@@ -21,6 +21,36 @@ impl NetworkValidator for String {
 
         ret
     }
+
+    /// Check if port is valid
+    /// # Arguments
+    /// * `port` - The port to check
+    /// # Example
+    /// ```
+    /// use rozmitmat::extensions::is_valid_port;
+    /// let port = "8080";
+    /// assert!(is_valid_port(port));
+    /// ```
+    /// # Returns
+    /// * `bool` - True if port is valid
+    fn is_valid_port(&self) -> bool {
+        let mut ret = true;
+        let port = self.clone();
+
+        //check if port is a number
+        if port.parse::<u16>().is_err() {
+            ret = false;
+        }
+
+        //check if port is in range 1024-65535
+        if let Ok(port) = port.parse::<u32>() {
+            if !(1024..=65535).contains(&port) {
+                ret = false;
+            }
+        }
+
+        ret
+    }
 }
 
 //create unit tests for this module
@@ -44,5 +74,26 @@ mod tests {
     fn test_is_invalid_ip_2() {
         let ip = "192.68.1.s".to_string();
         assert!(!ip.is_valid_ip());
+    }
+
+    #[test]
+    /// Test if port is valid
+    fn test_is_valid_port() {
+        let port = "8080".to_string();
+        assert!(port.is_valid_port());
+    }
+
+    #[test]
+    /// Test if port is invalid
+    fn test_is_invalid_port_1() {
+        let port = "808".to_string();
+        assert!(!port.is_valid_port());
+    }
+
+    #[test]
+    /// Test if port is invalid
+    fn test_is_invalid_port_2() {
+        let port = "80800".to_string();
+        assert!(!port.is_valid_port());
     }
 }
